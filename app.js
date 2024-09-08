2a212685f4ccc45eec9ebc3ed5b60529d2ecc1cf
Roger.Carvalho <143417647+RogerCarvalhoUEPB@users.noreply.github.com>
2024-09-07 21:32:18 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,47 @@
+function pesquisar() {
+
+    
+  // Obtém a seção onde os resultados serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
+  
+  let campoPesquisa= document.getElementById("campo-pesquisa").value;
+  console.log(campoPesquisa);
+
+
+  // Inicializa a variável para armazenar os resultados
+  let resultados = "";
+  let nome = "";
+  let endereco = "";
+  let descricao = "";
+  let tags = "";
+  if(!campoPesquisa){
+    section.innerHTML = "Busca vazia"
+    return;
+  }
+  campoPesquisa = campoPesquisa.toLowerCase();
+  
+    // Itera sobre cada hemocentro na lista 'hemocentros'
+    for (let dado of hemocentros) {
+        nome = dado.nome.toLowerCase();
+        endereco = dado.endereco.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags.toLowerCase();
+      // Constrói o HTML para cada resultado da pesquisa
+        if(nome.includes(campoPesquisa) ||endereco.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+            resultados += `
+            <div class="item-resultado">
+              <h2><a href="${dado.instagram}" target="_blank">${dado.nome}</a></h2>
+              <p class="descricao-meta">
+                ${dado.descricao} Localizado em ${dado.endereco}, telefone: ${dado.telefone} e funciona de ${dado.horarios}.
+              </p>
+            </div>
+          `;  
+        }
+    }if(!resultados){
+      resultados = "Nenhum resultado encontrado"
+    // Atualiza o conteúdo da seção com os resultados da pesquisa
+   
+  } section.innerHTML = resultados;
+}
+  
+  // Imprime o valor inicial da variável 'resultados' no console;
