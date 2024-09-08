34e851ef534d4a998b1aac84b4952606c166f27d
Roger.Carvalho <143417647+RogerCarvalhoUEPB@users.noreply.github.com>
2024-09-08 00:09:14 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 let hemocentros = [
     {
@@ -130,3 +131,137 @@ let hemocentros = [
 
 
 
+=======
+
+let hemocentros = [
+    {
+        nome: "Hemonúcleo - Patos",
+        endereco: "Rua Horácio Nóbrega, 100 - Centro",
+        telefone: "(83) 3421-2525",
+        descricao: "O Hemonúcleo de Patos é responsável pelo atendimento de pacientes de toda a região do Sertão Paraibano.",
+        horarios: "Segunda a sexta, das 7h às 17h",
+        instagram: "https://www.instagram.com/hemonucleo/",
+        tags: "hemonucleo, patos, paraiba"
+    },
+        {
+          nome: "Hemonúcleo de Belo Horizonte",
+          endereco: "Avenida Brasil, 123 - Centro",
+          telefone: "(31) 3234-5678",
+          descricao: "O Hemonúcleo de Belo Horizonte atende a capital mineira e região metropolitana.",
+          horarios: "Segunda a sexta, das 7h às 18h",
+          instagram: "",
+          tags: "hemonucleo, belo horizonte, minas gerais"
+        },
+        {
+          nome: "Hemocentro de Salvador",
+          endereco: "Rua da Bahia, 456 - Comércio",
+          telefone: "(71) 3331-6789",
+          descricao: "O Hemocentro de Salvador atende a capital baiana e região metropolitana.",
+          horarios: "Segunda a sexta, das 8h às 19h",
+          instagram: "",
+          tags: "hemocentro, salvador, bahia"
+        },
+        {
+          nome: "Hemonúcleo de Curitiba",
+          endereco: "Rua das Flores, 567 - Centro",
+          telefone: "(41) 3221-4567",
+          descricao: "O Hemonúcleo de Curitiba atende a capital paranaense e região metropolitana.",
+          horarios: "Segunda a sexta, das 7h às 17h",
+          instagram: "",
+          tags: "hemonucleo, curitiba, parana"
+        },
+        {
+          nome: "Hemocentro de Porto Alegre",
+          endereco: "Avenida Osvaldo Aranha, 123 - Centro",
+          telefone: "(51) 3345-6789",
+          descricao: "O Hemocentro de Porto Alegre atende a capital gaúcha e região metropolitana.",
+          horarios: "Segunda a sexta, das 8h às 18h",
+          instagram: "",
+          tags: "hemocentro, porto alegre, rio grande do sul"
+        },
+        {
+          nome: "Hemonúcleo de Manaus",
+          endereco: "Avenida Amazonas, 456 - Centro",
+          telefone: "(92) 3234-5678",
+          descricao: "O Hemonúcleo de Manaus atende a capital amazonense e interior do estado.",
+          horarios: "Segunda a sexta, das 7h às 17h",
+          instagram: "",
+          tags: "hemonucleo, manaus, amazonas"
+        },
+        {
+          nome: "Hemocentro de Goiânia",
+          endereco: "Rua Goiás, 123 - Centro",
+          telefone: "(62) 3345-6789",
+          descricao: "O Hemocentro de Goiânia atende a capital goiana e região metropolitana.",
+          horarios: "Segunda a sexta, das 8h às 18h",
+          instagram: "",
+          tags : "hemocentro, goiania, goias"
+        },
+        {
+          nome: "Hemonúcleo de Brasília",
+          endereco: "Avenida W3 Sul, 123 - Asa Sul",
+          telefone: "(61) 3331-6789",
+          descricao: "O Hemonúcleo de Brasília atende ao Distrito Federal.",
+          horarios: "Segunda a sexta, das 7h às 19h",
+          instagram: "",
+          tags: "hemonucleo, brasilia, distrito federal"
+        },
+        {
+          nome: "Hemocentro de São Luís",
+          endereco: "Avenida dos Holandeses, 456 - São Luís",
+          telefone: "(98) 3221-4567",
+          descricao: "O Hemocentro de São Luís atende a capital maranhense e interior do estado.",
+          horarios: "Segunda a sexta, das 8h às 18h",
+          instagram: "",
+            tags: "hemocentro, sao luis, maranhao"
+        },
+        {
+          nome: "Hemonúcleo de Belém",
+          endereco: "Avenida Nazaré, 123 - Centro",
+          telefone: "(91) 3345-6789",
+          descricao: "O Hemonúcleo de Belém atende a capital paraense e interior do estado.",
+          horarios: "Segunda a sexta, das 7h às 17h",
+          instagram: "",
+            tags: "hemonucleo, belem, para"
+        },
+        {
+          nome: "Hemocentro de Florianópolis",
+          endereco: "Avenida Beira Mar, 456 - Centro",
+          telefone: "(48) 3234-5678",
+          descricao: "O Hemocentro de Florianópolis atende a capital catarinense e região metropolitana.",
+          horarios: "Segunda a sexta, das 8h às 18h",
+          instagram: "",
+            tags: "hemocentro, florianopolis, santa catarina"
+        },
+        {
+          nome: "Hemonúcleo de Vitória",
+          endereco: "Avenida Vitória, 123 - Centro",
+          telefone: "(27) 3345-6789",
+          descricao: "O Hemonúcleo de Vitória atende a capital capixaba e região metropolitana.",
+          horarios: "Segunda a sexta, das 7h às 17h",
+          instagram: "",
+            tags: "hemonucleo, vitoria, espirito santo"
+        },
+        {
+          nome: "Hemocentro de Macapá",
+          endereco: "Avenida Feliciano Coelho, 456 - Centro",
+          telefone: "(96) 3221-4567",
+          descricao: "O Hemocentro de Macapá atende a capital amapaense e interior do estado.",
+          horarios: "Segunda a sexta, das 8h às 18h",
+          instagram: "",
+            tags: "hemocentro, macapa, amapa"
+        },
+        {
+          nome: "Hemocentro de Rio Branco",
+          endereco: "Avenida Getúlio Vargas, 123 - Centro",
+          telefone: "(68) 3234-5678",
+          descricao: "O Hemocentro de Rio Branco atende a capital acreana e interior do estado.",
+          horarios: "Segunda a sexta, das 7h às 17h",
+          instagram: "",
+            tags: "hemocentro, rio branco, acre"
+        }
+      ]
+
+
+
+>>>>>>> c199742abe38ce9595e193905f05807c4a5dcd19
---
+++ depoimentos.js
@@ -0,0 +1,17 @@
+let depoimentos = [
+      "Doar sangue é um ato de amor ao próximo. Senti uma paz incrível ao saber que ajudei a salvar uma vida." ,
+      "Receber sangue foi um presente de Deus. Graças à generosidade dos doadores, pude continuar minha vida." ,
+      "Doar sangue é simples e rápido, e faz toda a diferença para alguém." ,
+      "A sensação de ter ajudado a salvar uma vida não tem preço." ,
+      "Receber sangue foi um momento de esperança e gratidão." ,
+      "Doar sangue é um ato de cidadania que todos deveriam praticar." ,
+      "A doação de sangue me fez sentir parte de algo maior." ,
+      "Graças à transfusão, pude voltar a ter uma vida normal." ,
+      "Doar sangue é um ato de amor ao próximo e à vida." ,
+      "A doação de sangue é um presente que transforma vidas." ,
+      "Receber sangue foi um momento de renovação." ,
+      "Doar sangue é um ato de solidariedade que faz a diferença." ,
+      "A doação de sangue é um gesto simples que pode salvar muitas vidas." ,
+      "Graças à doação de sangue, pude realizar meus sonhos." ,
+      "Doar sangue é um ato de amor que me faz sentir completo." 
+];
